a296c5827715109b0b5480aee4941fb624187744
Meryem Bouras <bourasmeryem03@gmail.com>
2025-09-03 17:14:55 +0100

    ex8

--- challenge8.c
+++ challenge8.c
@@ -1,41 +1,25 @@
 #include <stdio.h>
-
 int main() {
-    int n; // Le nombre de termes à afficher
-    int first_term = 0;
-    int second_term = 1;
-    int next_term;
-
-    printf("Entrez le nombre de termes pour la suite de Fibonacci : ");
-    scanf("%d", &n);
-
-    // Gérer les cas où n est 0 ou 1
-    if (n <= 0) {
-        printf("Veuillez entrer un nombre positif.\n");
-        return 1;
+    int n; 
+    int t1, t2, tn;
+        printf("Veuillez saisir le nbr de termes  : ");
+        scanf("%d", &n);
+    t1 = 0;
+    t2 = 1;
+    printf("Les termes de cette suite de Fibonacci :\n");
+    if (n==0){
+         printf("%d", t1);
     }
-
-    printf("Suite de Fibonacci : ");
-
-    // Afficher les deux premiers termes (0 et 1)
-    if (n >= 1) {
-        printf("%d ", first_term);
+    else if (n==1){
+         printf("%d %d", t1, t2);
     }
-    if (n >= 2) {
-        printf("%d ", second_term);
+    else {
+        printf("%d %d" )
+    for (int i = 0; i < n; i++) {
+        tn = t1 + t2;
+        printf(" %d", tn); 
+        t1 = t2;
+        t2 = tn;
     }
-
-    // Boucle pour calculer et afficher les termes suivants
-    for (int i = 2; i < n; i++) {
-        next_term = first_term + second_term; // Le terme suivant est la somme des deux précédents
-        printf("%d ", next_term);
-
-        // Mettre à jour les valeurs pour la prochaine itération
-        first_term = second_term;
-        second_term = next_term;
-    }
-
-    printf("\n");
-
     return 0;
-}
+} }
